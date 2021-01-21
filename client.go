766515807e26bb5a06e671ed0c6d0df2a2452b07
Lewis Jessett <lewisje1991@gmail.com>
2021-01-21 22:37:59 +0000

    Client host validation passing

--- client.go
+++ client.go
@@ -1,9 +1,13 @@
 package client
 
+import "errors"
+
 type Client struct {
 	Host string
 }
 
 func NewClient(host string) (*Client, error) {
-	return nil, nil
+
+	return nil, errors.New("host cannot be empty")
+
 }
